a7c0b39f9e660319b616abde53abff4b6303dd78
Paulo Pontes <prsilvapontes@gmail.com>
2023-03-20 15:37:28 -0300

    lp4-2023 grid image

--- lib/view/tela_grid_imagens.dart
+++ lib/view/tela_grid_imagens.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../model/imagem.dart';
+import '../provider/provider_imagem.dart';
 
 class GridImagens extends StatefulWidget {
   const GridImagens({Key? key}) : super(key: key);
@@ -8,8 +12,21 @@ class GridImagens extends StatefulWidget {
 }
 
 class _GridImagensState extends State<GridImagens> {
+  List<Imagem> listaimg = [];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    listaimg = Provider.of<ImagemModel>(context, listen: true).listaImagem;
+
+    return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (context, index){
+          return Container(
+            padding: const EdgeInsets.all(8),
+            child: Image.network(listaimg[index].url),
+          );
+        },
+        itemCount: listaimg.length,
+    );
   }
 }
